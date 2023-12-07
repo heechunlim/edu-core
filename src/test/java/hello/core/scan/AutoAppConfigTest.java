00f2b41178abde587075a739129df02b33253758
HeeChun Lim <heechun_lim@tmaxsoft.co.kr>
2023-12-07 18:48:14 +0900

    필터

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -4,12 +4,13 @@ import hello.core.AutoAppConfig;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutoAppConfigTest {
     @Test
     void basicScan() {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService = context.getBean(MemberService.class);
         Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
